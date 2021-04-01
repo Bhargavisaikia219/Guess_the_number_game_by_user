945c572cb24b0d4bffd7b5161af867ed51687263
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-01 15:33:14 +0530

    Added new lines to the code

--- Guess_the_number_game_by_user.py
+++ Guess_the_number_game_by_user.py
@@ -1,14 +1,23 @@
 import random
 
-def guess(x):
-    random_number = random.randint(1, x)
+def guess(x,y):
+
+    random_number = random.randint(x, y)
+
     guess = 0
+
     while guess != random_number:
-        guess = int(input(f"Guess the number between 1 and {x}:"))
+        guess = int(input(f"Guess the number between {x} and {y}:"))
+
         if guess < random_number:
             print("Sorry. Guess again. hint: too low")
         elif guess > random_number:
             print("Sorry. Guess again. hint: too high")
+
     print(f"Hurray! You have guessed the number {random_number} correctly. ")
 
-guess(10)
+
+x=int(input("Enter a range between which you what to guess a random number: lower limit:"))
+y=int(input("Enter a range between which you what to guess a random number: upper limit:"))
+
+guess(x,y)
